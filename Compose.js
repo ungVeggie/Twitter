6fed896b9f10f5525e1f8875bd09f9ae6007abce
ungVeggie <33507917+ungVeggie@users.noreply.github.com>
2021-10-14 12:42:20 -0400

    Update Compose.js

--- Compose.js
+++ Compose.js
@@ -7,7 +7,7 @@ function Compose(props) {
             <div className="field">
                 <div className="control">
                     <Editor
-                        placeHolder={`Tweet something to the world!\n...Tell someone that you hate them\n\n...do it now`}
+                        placeHolder={`Tweet something to the world!\n...Tell someone that you love them\n\n...do it now`}
                         id={props.id}
                         event={"tweet"}
                         pushTweet={props.pushTweet}
