6be90a44d3b5a8ac80f6aa566effa0c2b5cd7eba
ungVeggie <33507917+ungVeggie@users.noreply.github.com>
2021-10-13 21:37:44 -0400

    Update and rename SubmitCancelButton.js to Components/Buttons/SubmitCancelButton.js

--- Components/Buttons/SubmitCancelButton.js
+++ Components/Buttons/SubmitCancelButton.js
@@ -1,7 +1,6 @@
 import API from '../../api';
 
 function SubmitCancelButton(props) {
-    console.log("props in submitcancel", props)
 
   return (
     <div className="field is-grouped is-grouped-right">
@@ -11,18 +10,15 @@ function SubmitCancelButton(props) {
             onClick={async (e) => {
                 e.stopPropagation();
                 let result = await API.create({message:props.message, id: props.id, type:"tweet"});
-                console.log(result)
-                //props.updateTweet(props.id, result.data.message)
+                props.updateTweet(props.id, result.data.message)
             }}
         >Submit</button>
         <button
             className="button is-danger is-light"
             onClick={async (e) => {
                 e.stopPropagation();
-                console.log("cancelling");
                 let result = await API.update(props.id, props.message);
-                console.log(result);
-                //props.updateTweet(result.data.id, result.data.body)
+                props.updateTweet(result.data.id, result.data.body)
             }}
         >Cancel</button>
     </div>
